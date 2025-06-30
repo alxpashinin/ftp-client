9a731730b73e7c6b79f6f88e3c0242ee89bde010
Alexander Pashinin <pashinin@Mac.lan>
2025-06-30 21:20:41 +0500

    Create [NewConnectionScreen] with business logic

--- lib/core/extensions/build_context.dart
+++ lib/core/extensions/build_context.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:ftp/core/l10n/gen/app_localizations.g.dart';
 
-extension BuildContextExtension on BuildContext {}
+extension BuildContextExtension on BuildContext {
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  AppLocalizations get loc => AppLocalizations.of(this)!;
+}
--- lib/core/widgets/material_app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyMaterialApp extends StatelessWidget {
-  const MyMaterialApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => const MaterialApp();
-}
